8213053ca707cb4f48fdec2079e3c54590c378e7
Alp Toker <tokeralp@gmail.com>
2022-10-03 17:52:31 +0300

    mobile2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-unused-expressions
 import "./styles/utilities.css";
 import "./styles/general.css";
+import "./styles/mobile.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import GettingStarted from "./pages/GettingStarted";
